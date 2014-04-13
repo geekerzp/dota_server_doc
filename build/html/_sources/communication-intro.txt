b49a496af00a3f2b82e8de3b960a6b7939cf5724
geekerzp <geekerzp@gmail.com>
2014-04-13 14:14:16 +0800

    增加api文档

---
+++ build/html/_sources/communication-intro.txt
@@ -0,0 +1,14 @@
+简介
+====
+*Protocol Buffers*
+  通信协议底层采用的是Google的Protocol Buffers，Protocol Buffers是一种用于序列化结构化
+  数据的机制，可用于分布式应用之间的数据通信，或者异构环境下的数据交互。
+  它具有灵活，高效，自动化的特点，类似于XML，但是比XML更小巧，快捷，简单。
+  在Google几乎它所有的内部RPC内部协议和文件格式都是采用PB。
+
+*Json*
+  通信协议的上层采用的是json。JSON(JavaScript Object Notation) 是一种轻量级的数据交换格式。
+  它基于JavaScript（Standard ECMA-262 3rd Edition - December 1999）的一个子集。
+  JSON采用完全独立于语言的文本格式，但是也使用了类似于C语言家族的习惯（包括C, C++, C#, Java, JavaScript, Perl, Python等）。
+  这些特性使JSON成为理想的数据交换语言。易于人阅读和编写，同时也易于机器解析和生成。初学者。
+
--- source/conf.py
+++ source/conf.py
@@ -100,7 +100,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# if on_rtd:
+#     html_theme = 'default'
+# else:
+#     html_theme = 'nature'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -258,3 +263,4 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
